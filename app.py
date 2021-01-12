aaee305fc8ceb932386a9693d6d75c2fcb3c6b04
Joey Hodson <joeyhodson32@gmail.com>
2021-01-12 17:37:31 -0500

    Added photos to project, activity pages. Slight syntax alterations.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, send_file
+MODULE_PATH = "/home/pi/.local/lib/python3.7/site-packages/flask_talisman/__init__.py"
+MODULE_NAME = "flask_talisman"
+import importlib
+import sys
+spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+module = importlib.util.module_from_spec(spec)
+sys.modules[spec.name] = module
+spec.loader.exec_module(module)
+
+from flask import Flask, render_template, send_file, redirect, request
+from flask_talisman import Talisman
 
 app = Flask(__name__)
+Talisman(app)
 
 @app.route('/')
 def index():
@@ -43,11 +54,12 @@ def robot():
     return render_template('robotQuad.html')
 
 @app.route('/JosephHodsonResume.pdf')
-def path():
+def pathToResume():
     try:
         return send_file('./JosephHodsonResume.pdf', attachment_filename='JosephHodsonResume.pdf')
     except Exception as e:
         return str(e)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=80, threaded=True)
+    app.run(host = '0.0.0.0', port=443, threaded=True, ssl_context=('/etc/letsencrypt/live/joeyhodson.com/fullchain.pem', '/etc/letsencrypt/live/joeyhodson.com/privkey.pem'))
+    #app.run(host = '0.0.0.0', port=80, threaded=True) http requests
